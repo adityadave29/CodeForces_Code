9e5676ba2531cf77cbee2a39aca8effab3d06dd0
Aditya Dave <cool2912004@gmail.com>
2022-09-20 17:27:16 +0530

    initial commit

---
+++ q4_cut_ribon.cpp
@@ -0,0 +1,71 @@
+/*
+
+Question Link : https://codeforces.com/problemset/problem/189/A
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                      long long 
+#define f                       first
+#define s                       second
+#define pb                      push_back
+#define pob                     pop_back
+#define mp                      make_pair
+#define vi                      vector<ll>
+#define pi                      pair<ll, ll>
+#define ti                      tuple<ll,ll,ll>
+#define si                      set<ll>
+#define msi                     multiset<ll>
+#define mod                     1000000007
+#define YES                     cout << "YES" << endl
+#define NO                      cout << "NO" << endl
+#define FIO                     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define rep(i, a, b)            for (ll(i) = a; (i) <=(b); (i)++)
+#define repe(i,a,b)             for(ll(i)=a; i<=(b); (i)--)
+#define endl                    '\n'
+#define sortt(v)                sort(v.begin(), v.end())
+#define rsortt(v)               sort(v.rbegin(), v.rend());
+#define c(x)                    cout << x << " "
+#define p(x)                    cout << x << endl
+#define SQ(a)                   a *a
+#define lb(x)                   lower_bound(v.begin(), v.end(), x)
+#define up(x)                   upper_bound(v.begin(), v.end(), x)
+#define er(x)                   equal_range(v.begin(), v.end(), x)
+#define all(v)                  v.begin(), v.end()
+
+void solve()
+{
+   int n,a,b,c,d;
+   cin >> n >> a >> b >> c;
+   int ans=0;
+   rep(i,0,4000){
+    rep(j,0,4000){
+        ll zc = n - (i*a + j*b);
+        if(zc<0){
+            break;
+        }
+        double z = (zc/(double)c);
+        if(z==(int)z){
+            ans = max(ans,(int)(z+i+j));
+        }
+    }   
+   }
+   cout << ans;
+}
+
+int32_t main()
+{
+    FIO;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // ll tomato;
+    ll tomato=1;
+    // cin >> tomato;
+    while(tomato--);
+    {
+        solve();
+    }
+    return 0;
+}
